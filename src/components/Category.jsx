8115f39a663bee5046026b715093d6b72b142b58
Eduardo Nakamura <edu-nakamura@hotmail.com>
2023-03-16 18:31:30 -0300

    nav links styling

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { FaPizzaSlice, FaHamburger } from 'react-icons/fa'
 import { GiNoodles, GiChopsticks } from 'react-icons/gi'
 import styled from "styled-components";
-
+import { NavLink } from 'react-router-dom';
 
 function Category() {
   return (
     <List>
-        <div>
+        <SLink to={'/cuisine/italian'}>
             <FaPizzaSlice />
             <h4>Italian</h4>
-        </div>
-        <div>
+        </SLink>
+        <SLink to={'/cuisine/american'}>
             <FaHamburger />
             <h4>American</h4>
-        </div>
-        <div>
+        </SLink>
+        <SLink to={'/cuisine/thai'}>
             <GiNoodles />
             <h4>Thai</h4>
-        </div>
-        <div>
+        </SLink>
+        <SLink to={'/cuisine/japanese'}>
             <GiChopsticks />
             <h4>Japanese</h4>
-        </div>
+        </SLink>
     </List>
   )
 }
@@ -31,4 +31,36 @@ const List = styled.div`
     justify-content: center;
     margin: 2rem 0rem
 `
+
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration:none;
+    cursor: pointer;
+    transform: scale(1);
+
+    h4{
+        color: ##bfbfbf;
+        font-size: 1.1rem;
+    }
+
+    svg{
+        color: ##bfbfbf;
+        font-size: 1.3rem;
+    }
+
+    &.active{
+        svg{
+            color: #00838F;
+        }
+        h4{
+            color: #00838F;
+        }
+    }
+`
+
 export default Category
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 function Cuisine() {
+    const [cuisine, setCuisine] = useState([])
+    let params = useParams();
+
+    const getCuisine = async (name) => {
+        console.log(name.type)
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=10&cuisine=${name.type}`)
+        const recipes = await data.json();
+        console.log(recipes)
+        setCuisine(recipes.results)
+    }
+
+    useEffect(() => {
+        getCuisine(params);
+    }, [params, params.type])
   return (
-    <div>Cuisine</div>
+    <Grid>
+        {cuisine.map((item) => {
+            return(
+                <Card key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <h4>{item.title}</h4>
+                </Card>
+            );
+        })}
+    </Grid>
   )
 }
 
+const Grid = styled.div`
+    display:grid;
+
+    grid-template-columns: repeat(auto-fit, minMax(10rem, 1fr));
+    grid-grap: 1rem
+`
+
+const Card = styled.div`
+    padding: 0.5rem;
+    img{
+        width: 100%;
+        border-radius:1rem
+    }
+    a{
+        text-decoration: none;
+    }
+    h4{
+        text-align:center;
+        padding: 1rem
+    }
+`
 export default Cuisine
